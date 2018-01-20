976a349c4702fbe7876708140005906ea47cddc5
ysk-tngc <ysk.tngc0505@gmail.com>
2018-01-20 13:16:46 +0900

    bind xml and activity

--- app/src/main/java/com/example/ysk/blockchainkotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/ysk/blockchainkotlinandroid/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.ysk.blockchainkotlinandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mineButton: Button
+    private lateinit var sendButton: Button
+    private lateinit var logView: TextView
+    private lateinit var chainView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mineButton = findViewById(R.id.mine_button)
+        mineButton.setOnClickListener {
+            onClickMineButton()
+        }
+
+        sendButton = findViewById(R.id.send_button)
+        sendButton.setOnClickListener {
+            onClickSendButton()
+        }
+
+        logView = findViewById(R.id.log_view)
+        chainView = findViewById(R.id.chain_view)
+    }
+
+    private fun onClickMineButton() {
+        Log.d("MainActivity", "onClickMineButton")
+    }
+
+    private fun onClickSendButton() {
+        Log.d("MainActivity", "onClickSendButton")
     }
 }
